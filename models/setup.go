10af4d4a34bfa5d7977a245b1d8589cc67e62782
necrotic7 <necrotic2971@gmail.com>
2022-01-12 17:49:53 +0800

    setup models

---
+++ models/setup.go
@@ -0,0 +1,31 @@
+package models
+
+import(
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+const(
+	USERNAME = "root"
+	PASSWORD = "80806789"
+	NETWORK = "tcp"
+	SERVER = "127.0.0.1"
+	PORT = 3306
+	DATABASE = "demo"
+)
+
+var DB *gorm.DB
+
+func ConnectDataBase(){
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",USERNAME,PASSWORD,NETWORK,SERVER,PORT,DATABASE)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil{
+		panic("connection error:"+err.Error())
+	}else{
+		fmt.Println("Connect success")
+	}
+
+	db.AutoMigrate(&Book{})
+	DB = db
+}
